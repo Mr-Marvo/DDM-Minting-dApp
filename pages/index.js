90380166c4ab04056ccc0333581f3a09ed443cf1
Nishan Premakumara <aknpremakumara@gmail.com>
2022-07-02 21:16:58 +0530

    UI Update

--- pages/index.js
+++ pages/index.js
@@ -88,35 +88,16 @@ export default function Home() {
 
       <div className="h-full w-full container max-w-5xl mx-auto flex flex-col items-center pt-4">
         <div className="flex flex-col items-center max-w-4xl w-full">
-          <Link href="/mint" passHref>
-            <a className="mt-16 font-coiny uppercase inline-flex items-center px-6 oy-2 text-sm sm:text-2xl md:text-3xl font-medium text-center rounded text-rose-500 hover:bg-rose-600 hover:text-white">
-              Go to minting page
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-6 h-6 ml-2 mt-0.5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M17 8l4 4m0 0l-4 4m4-4H3"
-                />
-              </svg>
-            </a>
-          </Link>
 
           <div className="flex flex-col md:flex-row md:space-x-16 space-y-10 items-center mt-20 w-full text-center">
             {/* BoredApe Image */}
             <img
               src="/images/9.png"
-              className="w-64 h-64 rounded-md object-cover"
+              className="w-72 h-72 rounded-md object-cover"
             />
 
             <div className="flex flex-col md:items-start items-center justify-center text-center font-coiny text-gray-800 px-4 md:px-0 py-10 mt-14">
-              <h2 className="font-bold text-2xl md:text-4xl uppercase">
+              <h2 className="font-bold text-2xl md:text-4xl uppercase ">
                 About DidemRaffe
               </h2>
 
@@ -131,6 +112,13 @@ export default function Home() {
               </p>
             </div>
           </div>
+
+          <Link href="/mint" passHref>
+            <a className="mt-16 font-coiny uppercase inline-flex items-center px-6 oy-2 text-sm sm:text-2xl md:text-3xl font-medium text-center rounded text-rose-500 hover:bg-rose-600 hover:text-white">
+              Mint Now
+            </a>
+          </Link>
+
         </div>
       </div>
     </div>
